3487f8a32f86e1d3c267a328feefcd08038c6db7
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-05-21 08:57:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void VOD()
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x} + {y} = {x+y}");
+        }
+        static void Main(string[] args)
+        {
+            VOD();
+            Console.Read();
+        }
+    }
+}
